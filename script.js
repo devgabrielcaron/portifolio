bf1d8551218e95a2d2d13f1d3b9b0c20169641ab
gabriel <gabri.caron@hotmail.com>
2021-03-22 20:32:34 -0300

    22/03

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
- document.querySelector(".hamburguer").addEventListener("click",() =>
-    document.querySelector(".container").classList.toggle("show-menu")
+ document.querySelector(".hamburguer").addEventListener("click",() =>{
+    document.querySelector(".container").classList.toggle("show-menu");
+    document.querySelector(".hamburguer").classList.add("hamburguer_ani_active")}
  );
